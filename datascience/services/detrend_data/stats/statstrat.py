da693ad1504fdae8b0ba36106ff50467dc4e1253
Aymane Cheikh <aymanecheikh2002@gmail.com>
2025-07-20 03:17:50 +0000

    autocorrelation tested and implemented. next steps are to compare and eval the stats, one step at a time. details to follow

--- datascience/services/detrend_data/stats/statstrat.py
+++ datascience/services/detrend_data/stats/statstrat.py
@@ -1,10 +1,22 @@
 from abc import ABC, abstractmethod
+from functools import cache
 import warnings
+import numpy as np
 
-from statsmodels.tsa.stattools import InterpolationWarning, ValueWarning, adfuller, kpss
+from statsmodels.stats.diagnostic import acorr_ljungbox
+from statsmodels.tsa.stattools import (
+    InterpolationWarning,
+    ValueWarning,
+    acf,
+    pacf,
+    adfuller,
+    kpss,
+)
+from statsmodels.tsa.tsatools import detrend
 
 warnings.simplefilter("ignore", InterpolationWarning)
 warnings.simplefilter("ignore", ValueWarning)
+
 
 class PureStatistic(ABC):
     @abstractmethod
@@ -40,6 +52,44 @@ class MeanValue(PureStatistic):
 
     def __str__(self) -> str:
         return "Mean"
+
+
+class AutoCorrelation(PureStatistic):
+    def acf_values(self, detrended_data):
+        return acf(detrended_data, nlags=30)[1:]
+    def pacf_values(self, detrended_data):
+        return pacf(detrended_data, nlags=30)[1:]
+    def ljungbox_min_pvalue(self, detrended_data):
+        return acorr_ljungbox(detrended_data, lags=[10, 20, 30], return_df=True)[
+            "lb_pvalue"
+        ].min()
+
+    def interpret_values(self, detrended_data):
+        return (
+            np.mean(np.abs(self.acf_values(detrended_data))),
+            np.mean(np.abs(self.pacf_values(detrended_data))),
+            np.max(np.abs(self.acf_values(detrended_data))),
+            np.max(np.abs(self.pacf_values(detrended_data))),
+        )
+    
+    def _normalize_inverse(self, x, max_acceptable=0.05):
+        return min(x / max_acceptable, 1.0)
+    def _normalize_pvalue(self, p, threshold=0.05):
+        return 1.0 - min(p / threshold, 1.0)
+    
+    def test_detrend(self, detrended_data) -> str:
+        return str(
+                sum(
+                    self._normalize_inverse(val) * 0.2
+                    for val in self.interpret_values(detrended_data)
+                    )
+                + self._normalize_pvalue(
+                    self.ljungbox_min_pvalue(detrended_data)
+                    )
+                )
+    
+    def __str__(self) -> str:
+        return "AutoCorrelation"
 
 
 class VarianceReduction(ComparativeStatistic):
